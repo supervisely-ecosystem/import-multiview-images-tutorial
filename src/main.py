6367dcc0043ef7ff7aedd1ed29b56f28f8747fd4
almaz <almazgimaev@gmail.com>
2023-12-14 11:29:26 +0100

    change listdir to scandir, remove constants

--- src/main.py
+++ src/main.py
@@ -12,8 +12,6 @@ workspace_id = sly.env.workspace_id()
 
 api = sly.Api.from_env()
 
-IMAGES_DIR = "src/images"
-
 project = api.project.create(workspace_id, "Grouped cars", change_name_if_conflict=True)
 dataset = api.dataset.create(project.id, "ds0")
 
@@ -25,31 +23,28 @@ api.project.set_multiview_settings(project.id)
 # ======================================================================================
 # OPTION 1: upload images as a group (recommended)
 
-for group_name in os.listdir(IMAGES_DIR):
-    group_dir = os.path.join(IMAGES_DIR, group_name)
-    if not os.path.isdir(group_dir):
+for group_dir in os.scandir("src/images"):
+    if not group_dir.is_dir():
         continue
-    images_paths = sly.fs.list_files(group_dir, valid_extensions=sly.image.SUPPORTED_IMG_EXTS)
+    images_paths = sly.fs.list_files(group_dir.path, valid_extensions=sly.image.SUPPORTED_IMG_EXTS)
 
-    api.image.upload_multiview_images(dataset.id, group_name, images_paths)
+    api.image.upload_multiview_images(dataset.id, group_dir.name, images_paths)
 
 # ======================================================================================
 # ======================================================================================
 # ======================================================================================
 # OPTION 2: upload images, add tags
 
-TAG_NAME = "multiview"
 # get project meta from server and tag meta (we will need it later)
 project_meta = sly.ProjectMeta.from_json(api.project.get_meta(project.id))
-tag_meta = project_meta.get_tag_meta(TAG_NAME)
+tag_meta = project_meta.get_tag_meta("multiview")
 
 # upload images to Supervisely
-for group_name in os.listdir(IMAGES_DIR):
-    group_dir = os.path.join(IMAGES_DIR, group_name)
-    if not os.path.isdir(group_dir):
+for group_dir in os.scandir("src/images"):
+    if not group_dir.is_dir():
         continue
-    images_paths = sly.fs.list_files(group_dir, valid_extensions=sly.image.SUPPORTED_IMG_EXTS)
+    images_paths = sly.fs.list_files(group_dir.path, valid_extensions=sly.image.SUPPORTED_IMG_EXTS)
     images_names = [os.path.basename(img_path) for img_path in images_paths]
     images_infos = api.image.upload_paths(dataset.id, images_names, images_paths)
     images_ids = [image_info.id for image_info in images_infos]
-    api.image.add_tag_batch(images_ids, tag_meta.sly_id, value=group_name)
+    api.image.add_tag_batch(images_ids, tag_meta.sly_id, value=group_dir.name)
